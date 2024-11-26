a879490e7345a91cd401a08a54f171441135251c
Troy Kubanka <118857635+Troy-Kubanka@users.noreply.github.com>
2024-11-25 22:57:20 -0500

    Add files via upload

---
+++ Push Up Plus/Push Up Plus/Push_Up_PlusApp.swift
@@ -0,0 +1,18 @@
+//
+//  Push_Up_PlusApp.swift
+//  Push Up Plus
+//
+//  Created by Troy Kubanka on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Push_Up_PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
